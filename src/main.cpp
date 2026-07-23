614572777a28f26dc6b84b189e82f7c84bfb127e
Carter Brashear <cbrashear@unr.edu>
2026-07-23 13:06:29 -0700

    Refined tech stack using OpenGL 3.3 Core and SDL3. Also using nlohmann's json.hpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <json.hpp>
+#include <SDL3/SDL.h>
+#include <glad.h>
+
+int main(int argc, char** argv) {
+	std::cout << "Hello, World!" << std::endl;
+
+	return 0;
+}
+
